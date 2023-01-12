2f0ad08126ffe52d6fb3d09fedb7d8fb09ea6e26
дом <amir/shakirov.2015@mail.ru>
2023-01-12 20:16:21 +0400

    Initial commit

---
+++ src/main/java/com/example/hwweb_3_recipe/HwWeb3RecipeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwweb_3_recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwWeb3RecipeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwWeb3RecipeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hwweb_3_recipe/HwWeb3RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwweb_3_recipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwWeb3RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
