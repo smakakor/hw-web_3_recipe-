f299c81e9c4c33a142918575c337f33b1f0a0a9b
дом <amir/shakirov.2015@mail.ru>
2023-01-14 22:33:23 +0400

    Подключение библиотек

--- src/main/java/com/example/hwweb_3_recipe/contoroller/IngredientController.java
+++ src/main/java/com/example/hwweb_3_recipe/contoroller/IngredientController.java
@@ -2,6 +2,10 @@ package com.example.hwweb_3_recipe.contoroller;
 
 import com.example.hwweb_3_recipe.model.Ingredient;
 import com.example.hwweb_3_recipe.service.IngredientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +14,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/ingredient")
+@Tag(name = "Ингредиенты", description ="Эндпоинты для работы с ингредиентами")
 public class IngredientController {
     private final IngredientService ingredientService;
 
@@ -18,12 +23,22 @@ public class IngredientController {
     }
 
     @PostMapping
+    @Operation(summary = "Добавление ингредиента")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Ингредиент добален"),
+            @ApiResponse(responseCode = "404",description = "Ингредиент не добален")
+    })
     public ResponseEntity<Ingredient> addIngredient(@RequestBody Ingredient ingredient) {
         ingredientService.addIngredient(ingredient);
         return ResponseEntity.ok(ingredient);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получения ингредиента", description = "Ингредиент можно получить вписав его id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Ингредиент найден"),
+            @ApiResponse(responseCode = "404",description = "Ингредиент не найден")
+    })
     public ResponseEntity<Ingredient> getIngredient(@PathVariable int id) {
         Ingredient ingredient = ingredientService.getIngredient(id);
         if (ingredient == null) {
@@ -33,6 +48,11 @@ public class IngredientController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Редактирование ингредиента", description = "Ингредиент можно отредоктировать указав его id и новый измененый ингредиент")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Ингредиент отредоктирован "),
+            @ApiResponse(responseCode = "404",description = "Ингредиент не удалось отредоктировать")
+    })
     public ResponseEntity<Ingredient> putIngredient(@PathVariable int id, @RequestBody Ingredient ingredient) {
         Ingredient ingredientVariable = ingredientService.putIngredient(id, ingredient);
         if (ingredientVariable == null) {
@@ -42,6 +62,11 @@ public class IngredientController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление ингредиента", description = "Ингредиент можно удалить вписав его id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Ингредиент удален"),
+            @ApiResponse(responseCode = "404",description = "Ингредиент не найден")
+    })
     public ResponseEntity<Void> deleteIngredient(@PathVariable int id) {
         if (ingredientService.deleteIngredient(id)) {
             return ResponseEntity.ok().build();
@@ -50,6 +75,11 @@ public class IngredientController {
     }
 
     @GetMapping
+    @Operation(summary = "Получение всех ингредиентов", description = "Получаем весь список ингредиентов, которые были добавлены")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Полученны все нгредиенты"),
+
+    })
     public Map<Integer, Ingredient> getAllIngredient() {
         return ingredientService.getAllIngredient();
     }
--- src/main/java/com/example/hwweb_3_recipe/contoroller/RecipeController.java
+++ src/main/java/com/example/hwweb_3_recipe/contoroller/RecipeController.java
@@ -3,6 +3,12 @@ package com.example.hwweb_3_recipe.contoroller;
 import com.example.hwweb_3_recipe.model.Ingredient;
 import com.example.hwweb_3_recipe.model.Recipe;
 import com.example.hwweb_3_recipe.service.RecipeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +16,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/recipe")
+@Tag(name = "Рецепты", description ="Эндпоинты для работы с рецепами")
 public class RecipeController {
     private final RecipeService recipeService;
 
@@ -18,12 +25,22 @@ public class RecipeController {
     }
 
     @PostMapping
+    @Operation(summary = "Добавление рецепта")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Рецепт добален"),
+            @ApiResponse(responseCode = "404",description = "Рецепт не добален")
+    })
     public ResponseEntity<Recipe> addRecipe(@RequestBody Recipe recipe) {
         recipeService.addRecipe(recipe);
         return ResponseEntity.ok(recipe);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Получения рецепта", description = "Рецепт можно получить вписав его id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Рецепт найден"),
+            @ApiResponse(responseCode = "404",description = "Рецепт не найден")
+    })
     public ResponseEntity<Recipe> getRecipe(@PathVariable int id) {
         Recipe recipe = recipeService.getRecipe(id);
         if (recipe == null) {
@@ -33,6 +50,11 @@ public class RecipeController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Редактирование рецепта", description = "Рецепт можно отредоктировать указав его id и новый измененый рецепт")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Рецепт отредоктирован "),
+            @ApiResponse(responseCode = "404",description = "Рецепт не удалось отредоктировать")
+    })
     public ResponseEntity<Recipe> putIngredient(@PathVariable int id, @RequestBody Recipe recipe) {
         Recipe recipeVariable = recipeService.putRecipe(id, recipe);
         if (recipeVariable == null) {
@@ -42,6 +64,11 @@ public class RecipeController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление рецепта", description = "Рецепт можно удалить вписав его id")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Рецепт удален"),
+            @ApiResponse(responseCode = "404",description = "Рецепт не найден")
+    })
     public ResponseEntity<Void> deleteRecipe(@PathVariable int id) {
         if (recipeService.deleteRecipe(id)) {
             return ResponseEntity.ok().build();
@@ -50,6 +77,11 @@ public class RecipeController {
     }
 
     @GetMapping
+    @Operation(summary = "Получение всех рецептов", description = "Получаем весь список рецептов, которые были добавлены")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200",description = "Полученны все рецепты"),
+
+    })
     public Map<Integer, Recipe> getAllIngredient() {
         return recipeService.getAllRecipe();
     }
